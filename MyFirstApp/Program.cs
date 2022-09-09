35c605b177bb607fce73d472f0e9bbad62951ada
Kristo K <kkorela@tktk.ee>
2022-09-09 19:04:00 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// This is a comment to my code
+
+// Rakendus küsib kasutajal sisestada tema nimi
+// Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+string userName;
+
+userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName!);
+
+Console.WriteLine($"Hello, {userName} !");
+
+
+
+
+
